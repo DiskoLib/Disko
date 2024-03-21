ae67cb5b48029ad5f0959b0c83b97e7a2c3dc605
Deftu <deftudev@gmail.com>
2024-03-21 18:10:38 +0200

    Add license header to all source files.

--- src/main/kotlin/dev/deftu/disko/entities/SelfUser.kt
+++ src/main/kotlin/dev/deftu/disko/entities/SelfUser.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2024 Deftu and the Disko contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package dev.deftu.disko.entities
 
 import dev.deftu.disko.utils.Snowflake
@@ -14,7 +32,6 @@ public class SelfUser(
     override val bot: Boolean = true
     override val system: Boolean = true
     override val avatar: String? = avatar
-        get() = field ?: "https://cdn.discordapp.com/embed/avatars/${(id.value shr 22) % 5}.png"
-
+        get() = field ?: "https://cdn.discordapp.com/embed/avatars/${(id.value shr 22) % 6}.png"
 
 }
