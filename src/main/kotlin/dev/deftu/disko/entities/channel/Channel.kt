c889a7a6d6fac1d5d1ca5c1f4c28615e336cdf07
Deftu <deftudev@gmail.com>
2024-04-04 00:03:22 +0200

    Add a base channel interface

---
+++ src/main/kotlin/dev/deftu/disko/entities/channel/Channel.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2024 Deftu and the Disko contributors
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package dev.deftu.disko.entities.channel
+
+import dev.deftu.disko.Disko
+import dev.deftu.disko.utils.Snowflake
+
+public interface Channel {
+    public val disko: Disko
+    public val shardId: Int
+
+    public val id: Snowflake
+    public val type: ChannelType
+
+    // TODO - Conversion functions for channel types
+
+    // TODO - fun isVisibleTo(user: User): Boolean
+}
