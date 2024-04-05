c087539c3c6a3529854c9f8bcc1d60d9da921d4a
Deftu <deftudev@gmail.com>
2024-04-05 12:05:57 +0200

    Add project display name to Gradle props

--- src/main/kotlin/dev/deftu/disko/DiskoBlock.kt
+++ src/main/kotlin/dev/deftu/disko/DiskoBlock.kt
@@ -23,7 +23,7 @@ import dev.deftu.disko.gateway.intents.IntentManager
 import dev.deftu.disko.presence.PresenceUpdateBuilder
 import okhttp3.OkHttpClient
 
-public class DiskoBuilder {
+public class DiskoBlock {
     public var httpClient: OkHttpClient? = null
     public var gatewayBuilder: ((Disko, Int) -> DiskoGateway)? = null
 
@@ -37,9 +37,6 @@ public class DiskoBuilder {
     public fun presence(block: PresenceUpdateBuilder.() -> Unit) {
         presenceBlock = block
     }
-
-    public fun build(): Disko =
-        Disko().also(::applyTo)
 
     internal fun applyTo(instance: Disko) {
         if (httpClient != null) instance.setHttpClient(httpClient!!)
